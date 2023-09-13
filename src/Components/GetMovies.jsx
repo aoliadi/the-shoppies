3140f83fb8cd2828641d8ce63e772960b156f283
Liadi Ahmed <aoliadi1998@gmail.com>
2023-09-13 18:11:05 +0100

    feat: :sparkles: searchQuery now fetched from ombd api

--- src/Components/GetMovies.jsx
+++ src/Components/GetMovies.jsx
@@ -1,6 +1,22 @@
-const GetMovies = ({ searchQuery }) => {
-  console.log(searchQuery);
-  return <div>GetMovies</div>;
+const GetMovies = ({ searchQuery, setSearchResults }) => {
+  // console.log(searchQuery);
+
+  const apikey = "74b088c9";
+
+  const handleFetch = () => {
+    fetch(`http://www.omdbapi.com/?s=${searchQuery}&apikey=${apikey}`)
+      .then((res) => res.json())
+      .then(({ Search }) => {
+        setSearchResults([...Search]);
+      });
+  };
+
+  return (
+    <>
+      <section>Get Movies</section>
+      <button onClick={handleFetch}>fetch now!</button>
+    </>
+  );
 };
 
 export default GetMovies;
