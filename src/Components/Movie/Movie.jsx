cc7ac8a8793f7811e0104c24ab9532a5859eb108
Liadi Ahmed <aoliadi1998@gmail.com>
2023-09-28 11:08:37 +0100

    style: :lipstick: Modified some styles for the movie card

--- src/Components/Movie/Movie.jsx
+++ src/Components/Movie/Movie.jsx
@@ -2,8 +2,9 @@ import card from "./card.module.css";
 import movie from "./movie.module.css";
 
 const Movie = ({ searchResults }) => {
-  const testMovieTitle = "Put Your Head on My Shoulder";
-  const yearOfRelease = 2023;
+  // const testMovieTitle = "Put Your Head on My Shoulder";
+  const testMovieTitle = "The Hobbit";
+  const yearOfRelease = 1977;
   const testImgLink =
     "https://m.media-amazon.com/images/M/MV5BN2ZmZGM3YTktOTk0Ni00Mjc4LThjYzEtYmExZGJiZjBlOTg3XkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_SX300.jpg";
 
@@ -20,7 +21,9 @@ const Movie = ({ searchResults }) => {
             className={card.container}
             style={{ backgroundImage: `url(${searchResult.Poster})` }}
           >
-            <h3 className={movie.title}>{searchResult.Title}</h3>
+            <h3 className={movie.title}>
+              <span>{searchResult.Title}</span>
+            </h3>
             <small className={movie.releaseDate}>{searchResult.Year}</small>
             <p className={movie.summary}>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
@@ -30,8 +33,9 @@ const Movie = ({ searchResults }) => {
           </li>
         ))}
         {/* <li className={card.container} style={bgStyles}>
-          <h3 className={movie.title}>{testMovieTitle}</h3>
-          <small className={movie.releaseDate}>{yearOfRelease}</small>
+          <h3 className={movie.title}>
+            <span>Lorem10</span>
+          </h3>
           <p className={movie.summary}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ullam
             repudiandae vel deleniti. Architecto eaque a, quibusdam quaerat
